1aa77ca7d68dc6f4cdc71026318a27914d67bc7d
unknown <safayas2019@gmail.com>
2024-07-22 11:36:55 +0530

    added the components

--- qtify/src/components/Navbar/Navbar.js
+++ qtify/src/components/Navbar/Navbar.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Button from "../Button/Button";
+// import { Link } from "react-router-dom";
+// import Button from "../Button/Button";
 import Logo from "../Logo/Logo";
-import Search from "../Search/Search";
+// import Search from "../Search/Search";
 import styles from "./Navbar.module.css";
 
 function Navbar({ searchData }) {
   return (
     <nav className={styles.navbar}>
-      <Link to="/">
+      {/* <Link to="/">
         <Logo />
-      </Link>
-      <Search
+      </Link> */}
+      {/* <Search
         placeholder="Search a song of your choice"
         searchData={searchData}
-      />
-      <Button>Give Feedback</Button>
+      /> */}
+      {/* <Button>Give Feedback</Button> */}
+      <h1>hi </h1>
     </nav>
   );
 }
